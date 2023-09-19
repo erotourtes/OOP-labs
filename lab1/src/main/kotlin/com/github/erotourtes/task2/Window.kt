21b647949bf2ad7078f89f3509146784275194e2
Max Siryk <erotourtes@gmail.com>
2023-09-19 12:18:59 +0300

    lab1: updated report

--- lab1/src/main/kotlin/com/github/erotourtes/task2/Window.kt
+++ lab1/src/main/kotlin/com/github/erotourtes/task2/Window.kt
@@ -6,32 +6,38 @@ import javafx.collections.ObservableList
 import javafx.scene.control.ListView
 import tornadofx.*
 
-class ListController : Controller() {
+class ListController(
+    private val fragment: Fragment,
+    private val observable: SimpleStringProperty
+) : Controller() {
     val items: ObservableList<String> = FXCollections.observableArrayList("IM-21", "IM-22", "IM-23", "IM-24")
-    private var message = ""
+    private var selectedMsg = ""
 
     fun handleChange(listview: ListView<String>) {
         listview.selectionModel.selectedItemProperty().addListener { _, _, newValue ->
-            message = newValue
+            selectedMsg = newValue
         }
     }
-    fun handleBtnChange(message: SimpleStringProperty) {
-        message.value = this.message
+
+    fun handleBtnYesChange() {
+        observable.value = this.selectedMsg
+    }
+
+    fun handleBtnCancelChange() {
+        observable.value = ""
+        fragment.close()
     }
 }
 
 class Window : Fragment("Window") {
     val message: SimpleStringProperty by param()
-    private val controller = ListController()
+    private val controller = ListController(this, message)
 
     override val root = vbox {
         listview(controller.items) { controller.handleChange(this) }
         hbox {
-            button("Show") { action { controller.handleBtnChange(message) } }
-            button("Cancel") { action {
-                message.value = ""
-                close()
-            } }
+            button("Yes") { action(controller::handleBtnYesChange) }
+            button("Cancel") { action(controller::handleBtnCancelChange) }
         }
     }
 }
