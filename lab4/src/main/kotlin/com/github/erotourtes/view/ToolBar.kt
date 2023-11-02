f666d926c120cb9fc171af98cfb280ee3898e04c
Max Siryk <erotourtes@gmail.com>
2023-11-02 15:04:59 +0200

    added(toolbar): attach/detach logic

--- lab4/src/main/kotlin/com/github/erotourtes/view/ToolBar.kt
+++ lab4/src/main/kotlin/com/github/erotourtes/view/ToolBar.kt
@@ -3,14 +3,25 @@ package com.github.erotourtes.view
 import com.github.erotourtes.drawing.editor.EmptyEditor
 import com.github.erotourtes.styles.ToolbarStyles
 import com.github.erotourtes.utils.EditorInfo
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView
 import javafx.scene.control.ToggleButton
 import javafx.scene.control.ToggleGroup
+import javafx.scene.layout.BorderPane
+import javafx.stage.StageStyle
 import tornadofx.*
 
 class ToolBarController : Controller() {
+    val detached
+        get() = isDetached
     private val model: CanvasModel by inject()
     private val group = ToggleGroup()
+    private lateinit var toolBar: ToolBar
+    private var isDetached = false
+
+    fun setView(view: ToolBar) {
+        this.toolBar = view
+    }
 
     fun create() = model.ei.map {
         ToggleButton().apply {
@@ -38,17 +49,44 @@ class ToolBarController : Controller() {
             }
         }
     }
+
+    fun toggle() = if (isDetached) attach() else detach()
+
+    private fun attach() {
+        isDetached = false
+        val center = find<MainView>().root.center as BorderPane
+        toolBar.close()
+        center.top = toolBar.root
+    }
+
+    private fun detach() {
+        isDetached = true
+        toolBar.removeFromParent()
+        toolBar.openWindow(StageStyle.UTILITY)
+    }
 }
 
 class ToolBar : View() {
     private val ctrl: ToolBarController by inject(super.scope)
 
     override fun onDock() {
+        ctrl.setView(this)
         ctrl.listenToEditorChange()
     }
 
     override val root = hbox {
         addClass(ToolbarStyles.toolbar)
+        button {
+            addClass(ToolbarStyles.iconButton)
+            val detach = FontAwesomeIconView(FontAwesomeIcon.UNLINK)
+            val attach = FontAwesomeIconView(FontAwesomeIcon.LINK)
+            graphic = detach
+
+            action {
+                graphic = if(ctrl.detached) detach else attach
+                ctrl.toggle()
+            }
+        }
         ctrl.create().forEach { add(it) }
     }
 }
